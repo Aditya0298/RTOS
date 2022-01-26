8976c67ba425b517f558f7301fb976c34bc87595
Aditya0298 <adityakaushik532@gmail.com>
2022-01-26 21:10:32 +0530

    assignment question 7

---
+++ assignments/q7/main.c
@@ -0,0 +1,16 @@
+/*ques=What is the scheduling algorithm used by your RTOS?
+answer = sheludling algorithms tells us that which will choose between the priority
+which will go execution i.e low priority and high priority in rtos it will always choose high 
+priority
+. types of scheduling algorithms are:
+1.fifo= a thread selected to run continue execution until it
+~voluntarily block
+~is preempted by a higher priority thread
+2.earliest deadline first.
+~earliest deadline first or least time to go is a dynamic priorty scheduling algorith used in 
+RTOS.
+3.rate monotonic scheduling
+~it is a priority assignment algorithm 
+4. static priortiy driven scheduling
+~Most hard real-time systems built and in use to date are static. In the case when tasks in a
+ static system are independent, we consider scheduling jobs to a single processor

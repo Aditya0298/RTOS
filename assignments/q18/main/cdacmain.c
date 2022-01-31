06328f7d6ddcd6f2d5a44882ed89e53c17224bec
Aditya0298 <adityakaushik532@gmail.com>
2022-01-31 09:35:22 +0530

    assginments question

--- assignments/q18/main/cdacmain.c
+++ assignments/q18/main/cdacmain.c
@@ -4,32 +4,31 @@
 #include <freertos/queue.h>
 
 QueueHandle_t hello_queue;
-
+int intsend =5;
 void task1(void *pv)
 {
-    int result1 = 0;
     while(1)
     {
-        result1++;
-        printf("the queue one is here %d/n",result1);
-xQueueSend(elephant,task1,portMAX_DELAY);
+        intsend++;
+        printf("the queue one is here %d/n",intsend);
+xQueueSend("elephant",task1,portMAX_DELAY);
  vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 void task2(void *pv)
 {
-    int result2 = 0;
+    //int result2 = 0;
     while(1)
     {
-        result2++;
-        printf("queue two is here %d\n",result2);
+      //  result2++;
+        //printf("queue two is here %d\n",result2);
         xQueueReceive(hello_queue,task2,portMAX_DELAY);
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 void app_main()
 {
-    hello_queue = xQueueCreate(20,sizeof(int));
+    hello_queue = xQueueCreate(5,sizeof(int));
     xTaskCreate(task1,"hello",2048,NULL,5,NULL);
     xTaskCreate(task2,"hello",2048,NULL,3,NULL);
     printf("hello aditya/n");
